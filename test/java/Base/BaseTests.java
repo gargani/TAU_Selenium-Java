c8ede73bc013aae1c0bdfb5ab36295574196c736
Anirudh Garg <anirudh.garg92@gmail.com>
2019-12-26 03:34:41 +0530

    This is commit has following additions: 1. Added code to handle SSL certificates for Chrome, Firefox and Safari browsers.

--- test/java/Base/BaseTests.java
+++ test/java/Base/BaseTests.java
@@ -3,6 +3,9 @@ package Base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -18,32 +21,46 @@ public class BaseTests {
 
     @BeforeClass
     public void Setup() {
+        String os_Name = System.getProperty("os.name").toLowerCase();
+        String currentDirectory = System.getProperty("user.dir").toLowerCase();
+        String java_Version = System.getProperty("java.version").toLowerCase();
 
-        String OS_Name = System.getProperty("os.name").toLowerCase();
-        System.out.println(OS_Name);
+        System.out.println(os_Name);
+        System.out.println(currentDirectory);
+        System.out.println(java_Version);
 
-        switch (driverName.toLowerCase()) {
-            case "chrome":
-                //As per my Machine/MacBook
-                System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
-                driver = new ChromeDriver();
-                driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
-                break;
-            //Just Add Maven dependency, no need to download SafariDriver separately.
+        if (os_Name.contains("mac") && driverName == "safari") {
+            switch (driverName.toLowerCase()) {
+                case "chrome":
+                    //Code to handle SSL certifcates (if any)
+                    DesiredCapabilities capChrome = DesiredCapabilities.chrome();
+                    capChrome.setCapability(CapabilityType.ACCEPT_SSL_CERTS,true);
+                    //As per my Machine/MacBook
+                    System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+                    driver = new ChromeDriver();
+                    driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+                    break;
+                //Just Add Maven dependency, no need to download SafariDriver separately.
+                case "safari":
+                    //Code to handle SSL certifcates (if any)
+                    DesiredCapabilities capSafari = DesiredCapabilities.safari();
+                    capSafari.setCapability(CapabilityType.ACCEPT_SSL_CERTS,true);
+                    System.setProperty("webdriver.safari.driver", "true");
+                    driver = new SafariDriver();
+                    driver.manage().timeouts().implicitlyWait(800, TimeUnit.MILLISECONDS);
+                    break;
 
-            case "safari":
-                System.setProperty("webdriver.safari.driver", "true");
-                driver = new SafariDriver();
-                driver.manage().timeouts().implicitlyWait(800, TimeUnit.MILLISECONDS);
-                break;
+                case "firefox":
+                    //Code to handle SSL certifcates (if any)
+                    FirefoxProfile profile = new FirefoxProfile();
+                    profile.setAcceptUntrustedCertificates(true);
 
-            case "firefox":
-                System.setProperty("webdriver.gecko.driver", "resources/geckodriver");
-                driver = new FirefoxDriver();
-                driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
-                break;
+                    System.setProperty("webdriver.gecko.driver", "resources/geckodriver");
+                    driver = new FirefoxDriver();
+                    driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+                    break;
+            }
         }
-
 
         driver.get(url);
         driver.manage().window().maximize();
